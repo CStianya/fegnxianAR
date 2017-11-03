5164491d82f49648f8339fec8738979bfb616427
Kaloneme <879057619@qq.com>
2017-11-03 11:17:05 +0800

    完善双摄问题

--- app/src/main/java/com/riskmanagement/activity/Form2Activity.java
+++ app/src/main/java/com/riskmanagement/activity/Form2Activity.java
@@ -2,11 +2,18 @@ package com.riskmanagement.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.zhaoshuang.weixinrecordeddemo.SMainActivity;
+import com.example.zhaoshuang.weixinrecordeddemo.StartActivity;
+import com.example.zhaoshuang.weixinrecordeddemo.session.SessionKeeper;
 import com.riskmanagement.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
 
 /**
  * Created by CS on 2017/10/23.
@@ -22,7 +29,6 @@ public class Form2Activity extends BaseActivity {
         setContentView(R.layout.activity_form_2);
         initTop("交底记录");
         initViews();
-        initDatas();
     }
     //初始化Views
     private void initViews(){
@@ -30,13 +36,31 @@ public class Form2Activity extends BaseActivity {
         btn_ARShow_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                SessionKeeper.setInstitution("9f28cacfb6e54c2798587b00bbbffaa5", getApplicationContext());
+                SessionKeeper.setWarranty(createWarranty(), getApplicationContext());
+                SessionKeeper.setUseName("admin", getApplicationContext());
                 startActivity(new Intent(Form2Activity.this, SMainActivity.class));
                 finish();
             }
         });
     }
-    //初始化数据
-    private void initDatas() {
 
+    private String createWarranty() {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+        Date curDate = new Date(System.currentTimeMillis());
+        String time = formatter.format(curDate);
+        Log.e("curTime", time);
+        StringBuffer sb = new StringBuffer();
+        sb.append(time);
+        byte length = 5;
+        String base = "0123456789";
+        Random random = new Random();
+
+        for(int i = 0; i < length; ++i) {
+            int number = random.nextInt(base.length());
+            sb.append(base.charAt(number));
+        }
+        return sb.toString();
     }
+
 }
